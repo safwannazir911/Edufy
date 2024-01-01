12fde90d6725c3dde581d2bca9876cc3d4083ffb
Bhat Aasim <bhataasim700@gmail.com>
2024-01-01 19:54:53 +0530

    added cors in main.py

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from models.create_table import init_table
 from routers import users, posts
@@ -7,6 +8,17 @@ app = FastAPI()
 
 # Initialize table
 init_table()
+
+# Set up CORS
+origins = ["*"]  # You can specify your frontend's URL instead of "*"
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # You can specify the allowed HTTP methods
+    allow_headers=["*"],  # You can specify the allowed HTTP headers
+)
 
 app.include_router(users.router)
 app.include_router(posts.router)
